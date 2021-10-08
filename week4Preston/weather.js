6f3a51a8aaedbafa933adde26edb014e805b6ee1
mlutz060 <lut19003@byui.edu>
2021-10-08 12:22:57 -0600

    responsivenav

--- week4Preston/weather.js
+++ week4Preston/weather.js
@@ -1,7 +1,16 @@
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+};
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
 
-
-function toggleMenu(
-    
-)
+function toggleMenu() {
+    var hamburger = document.querySelector("#myTopnav");
+    if (hamburger.className === "topnav") {
+        hamburger.className += " responsive";
+    } else {
+        hamburger.className = "topnav";
+    }
+}
