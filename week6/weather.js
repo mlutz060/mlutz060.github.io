0a39298aa21b47801d8127c8785caea204e7bdc0
mlutz06 <madison.lutz@sentinelofsafety.com>
2021-10-23 17:28:54 -0600

    images

--- week6/weather.js
+++ week6/weather.js
@@ -48,8 +48,31 @@ function banner() {
 
 function windchill() {
     let chill = document.querySelector('.chill')
-    let temp = 54;
-    let wind = 12;
-    let windChill = (wind * .07) - 54;
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
+    wc = Math.floor(wc);
+    wc = (wc > temp)?temp:wc;
+    wcTemp.innerHTML = wc;
     document.chill.innerHTML(windChill);
 };
+
+
+ // Calculate the Windchill
+ function buildWC(speed, temp) {
+    let wcTemp = document.getElementById('windchill');
+   
+    // Compute the windchill
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
+   
+    // Round the answer down to integer
+    wc = Math.floor(wc);
+   
+    // If chill is greater than temp, return the temp
+    wc = (wc > temp)?temp:wc;
+   
+    // Display the windchill
+    console.log(wc);
+    wc = wc+'°F';
+    wcTemp.innerHTML = wc;
+    }
