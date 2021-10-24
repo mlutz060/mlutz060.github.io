bc98e9f3514dbada837e08f584cee1a35661d865
mlutz06 <madison.lutz@sentinelofsafety.com>
2021-10-23 22:17:03 -0600

    ugi

--- week6/weather.js
+++ week6/weather.js
@@ -62,6 +62,8 @@ function windchill() {
     let wcTemp = document.getElementById('windchill');
    
     // Compute the windchill
+    let temp = 54;
+    speed = 12;
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     console.log(wc);
    
