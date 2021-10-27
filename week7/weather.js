cb462948694e3e139d2bb9d87e620700dae15f2f
mlutz06 <madison.lutz@sentinelofsafety.com>
2021-10-27 11:38:58 -0600

    fixing everything

--- week7/weather.js
+++ week7/weather.js
@@ -46,15 +46,15 @@ function banner() {
 };
 
 
-// function windchill() {
-//     let chill = document.querySelector('.chill')
-//     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
-//     console.log(wc);
-//     wc = Math.floor(wc);
-//     wc = (wc > temp)?temp:wc;
-//     wcTemp.innerHTML = wc;
-//     document.chill.innerHTML(windChill);
-// };
+function windchill() {
+    let chill = document.querySelector('.chill')
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+    console.log(wc);
+    wc = Math.floor(wc);
+    wc = (wc > temp)?temp:wc;
+    wcTemp.innerHTML = wc;
+    document.chill.innerHTML(windChill);
+};
 
 
  // Calculate the Windchill
@@ -62,7 +62,8 @@ function banner() {
     let wcTemp = document.getElementById('windchill');
    
     // Compute the windchill
-
+    let temp = 54;
+    speed = 12;
     let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     console.log(wc);
    
@@ -76,6 +77,4 @@ function banner() {
     console.log(wc);
     wc = wc+'°F';
     wcTemp.innerHTML = wc;
-    };
-
-    buildWC(12, 54);
+    }
