549744e071363f145359fecfb2ed1f0629ecd313
mlutz06 <madison.lutz@sentinelofsafety.com>
2021-11-06 21:13:59 -0600

    final

---
+++ week8/script.js
@@ -0,0 +1,67 @@
+function lastSave(){
+    const toisoString = new Date(document.lastModified).toISOString();
+
+    const options = {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+        weekday: "long"
+    };
+    
+    console.log(toisoString);
+    const date = new Date(toisoString);
+    const upDate = new Intl.DateTimeFormat("en-US", options).format(date);
+
+    const lastModified = document.getElementById("date");
+
+    lastModified.innerText = "Last updated: " + upDate;
+};
+
+lastSave();
+
+
+//the event listener is onclick in the HTML
+
+function toggleMenu() {
+    var hamburger = document.querySelector("#myTopnav");
+    if (hamburger.className === "topnav") {
+        hamburger.className += " responsive";
+    } else {
+        hamburger.className = "topnav";
+    }
+};
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+const imgOptions = {
+  threshold: 0.1
+}
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+
+if ("IntersectionObserver" in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  }, imgOptions);
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
+
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
