4c6dd5952ca7976003b9680ef2ac60c1d831bae9
mlutz06 <madison.lutz@sentinelofsafety.com>
2021-11-10 10:45:56 -0700

    hsh

---
+++ week9/prohpet.js
@@ -0,0 +1,39 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+    .then(function (response) {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new ERROR('Network response was not ok');
+    })
+
+    .then(function (jsonObject) {
+            console.table(jsonObject);
+            const prophets = jsonObject['prophets'];
+
+            for (let i = 0; i < prophets.length; i++) {
+                let card = document.createElement('section');
+                let h2 = document.createElement('h2');
+                let birthDate = document.createElement('p');
+                let birthPlace = document.createElement('p');
+                let image = document.createElement('img');
+
+                h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+                card.appendChild(h2);
+                birthDate.textContent = "Place of Birth: " + prophets[i].birthplace;
+                card.appendChild(birthPlace)
+                birthDate.textContent = "Date of Birth: " + prophets[i].birthdate;
+                card.appendChild(birthDate);
+                image.setAttribute('src', prophets[i].imageurl);
+                image.setAttribute('alt', prophets[i].name + ' ' + prophets[i].lastname + " - " + (i+1));
+                card.appendChild(image);
+
+                document.querySelector('div.cards').appendChild(card);
+            }
+            })
+
+            .catch(function (error) {
+                console.log('Fetch error: ', error.message);
+            })
+ 
