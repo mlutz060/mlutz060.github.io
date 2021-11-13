7de1e3553eb689e17206b5e5e4f70fb43e2ccc95
mlutz06 <madison.lutz@sentinelofsafety.com>
2021-11-13 12:02:49 -0700

    json

--- week9/prohpet.js
+++ week9/prohpet.js
@@ -1,6 +1,6 @@
-const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
-fetch(requestURL)
+
+fetch(json)
     .then(function (response) {
         if (response.ok) {
             return response.json();
